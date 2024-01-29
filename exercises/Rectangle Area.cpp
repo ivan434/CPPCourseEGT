660080c4fced394ecbacc76dd95c444aa64d6021
ivan434 <61167268+ivan434@users.noreply.github.com>
2024-01-29 22:29:13 +0200

    Create Rectangle Area.cpp

---
+++ exercises/Rectangle Area.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Rectangle {
+private:
+    double length;
+    double width;
+
+public:
+    // Constructor
+    Rectangle(double len, double wid) : length(len), width(wid) {}
+
+    // Member functions to calculate area and perimeter
+    double calculateArea() const {
+        return length * width;
+    }
+
+    double calculatePerimeter() const {
+        return 2 * (length + width);
+    }
+};
+
+int main() {
+    // Input the dimensions of the rectangle
+    double length, width;
+    std::cout << "Enter the length of the rectangle: ";
+    std::cin >> length;
+    std::cout << "Enter the width of the rectangle: ";
+    std::cin >> width;
+
+    // Create a rectangle object
+    Rectangle rectangle(length, width);
