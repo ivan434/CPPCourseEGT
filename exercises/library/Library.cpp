975a20bd282add84bbc80408905c5c180c7c136c
ivan434 <61167268+ivan434@users.noreply.github.com>
2024-01-29 20:00:47 +0200

    Create Library.cpp

---
+++ exercises/library/Library.cpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std ;
+
+
+class Author {
+public:
+    // Constructor
+    Author(const string& fName, const string& lName, const string& nationality)
+        : firstName(fName), lastName(lName), nationality(nationality) {}
+
+    // Getter functions
+    string getFirstName() const { return firstName; }
+    string getLastName() const { return lastName; }
+    string getNationality() const { return nationality; }
+
+    // Setter functions
+    void setFirstName(const string& fName) { firstName = fName; }
+    void setLastName(const string& lName) { lastName = lName; }
+    void setNationality(const string& n) { nationality = n; }
+
+    // Display function
+    void print() const {
+        cout << firstName << " " << lastName << " " << nationality << endl;
+    }
+
+private:
+    // Private member variables
+    string firstName;
+    string lastName;
+    string nationality;
+};
