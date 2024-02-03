da31aaf26798ced453388ee3f8afde07fe778fa8
ivan434 <61167268+ivan434@users.noreply.github.com>
2024-02-03 19:10:01 +0200

    Create Family_Car_Homework.cpp

---
+++ exercises/Car_Homework/Family_Car_Homework.cpp
@@ -0,0 +1,35 @@
+#include "FamilyCar.h"
+
+FamilyCar::FamilyCar(string brand, string model, string type, string VIN, string plateNo, int dist)
+:Car(brand, model, type, VIN, plateNo)
+{
+    setDistanceTravelled(dist);
+}
+
+double FamilyCar::calcTax(int dist)
+{
+    if (dist < 500){
+        return dist*0.7;
+    }
+    else {
+        return dist*0.4;
+    }
+    
+}
+
+void FamilyCar::setDistanceTravelled(int dist)
+{
+    this->distanceTravelled = dist;
+}
+
+int FamilyCar::getDistanceTravelled()
+{
+    return this->distanceTravelled;
+}
+
+void FamilyCar::print()
+{
+    cout << "Family car: " << endl;
+    cout << getBrand() << " " << getModel() << " " << getType() << " VIN: "<< getVIN() << " " << getPlateNo() << endl;
+    cout << "Total charge: " << calcTax(getDistanceTravelled()) << endl;
+}
