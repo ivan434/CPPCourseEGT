166216ae542c678ecddf4bb874b9df80a8379d3a
ivan434 <61167268+ivan434@users.noreply.github.com>
2024-02-03 19:14:53 +0200

    Create Main_homework.cpp

---
+++ exercises/Car_Homework/Main_homework.cpp
@@ -0,0 +1,14 @@
+include 
+
+LuxuryCar
+
+nclude "FamilyCar.h"
+#include <vector>
+
+//TODO 
+//ADD COLOR TO CAR CLASS
+//ADD TAX FOR DAILY USE TO CAR CLASS
+
+int main(){
+
+    vector<Car *> cars;
